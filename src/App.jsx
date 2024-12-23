bc411c8fd80b4ddb07d0b60293fcf09061528cf2
sahilajaz <sahilajazw@gmail.com>
2024-12-23 23:12:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+
+function App() {
+    return (
+        <main className="max-w-7xl mx-auto">
+          <Navbar/>
+           <Hero/>
+        </main>
+    )
+}
+
+export default App
